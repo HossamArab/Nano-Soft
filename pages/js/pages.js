11cfa51fab5173c71a67f66ef0f345acb6c12aab
HossamArab <96493106+HossamArab@users.noreply.github.com>
2022-10-11 09:17:19 +0200

    0

--- pages/js/pages.js
+++ pages/js/pages.js
@@ -5,6 +5,7 @@ var btn = document.getElementById("myBtn");
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
+var Video = document.getElementById("Video");
 
 // When the user clicks on the button, open the modal
 btn.onclick = function () {
@@ -14,11 +15,26 @@ btn.onclick = function () {
 // When the user clicks on <span> (x), close the modal
 span.onclick = function () {
     modal.style.display = "none";
+    stopVideo(document);
 }
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function (event) {
     if (event.target == modal) {
         modal.style.display = "none";
+        stopVideo(document);
     }
 }
+
+
+function stopVideo(element) {
+         // getting every iframe from the body
+         var iframes = element.querySelectorAll('iframe');
+         // reinitializing the values of the src attribute of every iframe to stop the YouTube video.
+         for (let i = 0; i < iframes.length; i++) {
+            if (iframes[i] !== null) {
+               var temp = iframes[i].src;
+               iframes[i].src = temp;
+            }
+         }
+      };
